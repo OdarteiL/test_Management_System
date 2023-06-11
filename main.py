c2917e256e558540c8a051a8fcebcac375a6c4b0
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2023-06-11 14:04:54 +0100

    latest

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-class cars: 
-    def __init__(self, brand, model):
+class french:
+    def say_hello(self):
+        print("Bonjour")
+        
+class chinese:
+    def say_hello(self):
+        print("Ni hoe!")
+        
+def greet(lang):
+    lang.say_hello()
+
+
+Mike = french()
+John = chinese()
+
+greet(Mike)
+greet(John)
+
+
+# ------------------- Cars and Brand ---------------------
+
+
+class cars:
+    def __init__ (self, brand, model):
         self.brand = brand
-        self.model = model 
-    
-    def car(self):
-        return (f'{brand} : {model}')
+        self.model = model
+        
+    def car_details(self):
+        return (f'{self.brand} and {self.model}')
 
+car_1 = cars("Mistibushi", "Carisma")
 
-Japan_car = cars("Honda", "civic")
-
-Japan_car.car()
+print(car_1.car_details())
